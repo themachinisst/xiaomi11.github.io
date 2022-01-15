593683c1da4102171c72ad33fce07dba537d2663
Shubham Jadhav <shub13711jadhav@gmail.com>
2022-01-15 13:18:58 +0530

    Update tic.js

--- tic.js
+++ tic.js
@@ -25,7 +25,9 @@ function myfunc() {
         document.getElementById("b7").disabled = true;
         document.getElementById("b8").disabled = true;
         document.getElementById("b9").disabled = true;
+
         window.alert('Player X won');
+        myfunc_2();
     }
     else if ((b1 == 'x' || b1 == 'X') && (b4 == 'x' ||
         b4 == 'X') && (b7 == 'x' || b7 == 'X')) {
@@ -39,6 +41,7 @@ function myfunc() {
         document.getElementById("b9").disabled = true;
  
         window.alert('Player X won');
+        myfunc_2();
     }
     else if ((b7 == 'x' || b7 == 'X') && (b8 == 'x' ||
         b8 == 'X') && (b9 == 'x' || b9 == 'X')) {
@@ -50,7 +53,9 @@ function myfunc() {
         document.getElementById("b4").disabled = true;
         document.getElementById("b5").disabled = true;
         document.getElementById("b6").disabled = true;
+        
         window.alert('Player X won');
+        myfunc_2();
     }
     else if ((b3 == 'x' || b3 == 'X') && (b6 == 'x' ||
         b6 == 'X') && (b9 == 'x' || b9 == 'X')) {
@@ -63,6 +68,7 @@ function myfunc() {
         document.getElementById("b7").disabled = true;
         document.getElementById("b8").disabled = true;
         window.alert('Player X won');
+        myfunc_2();
     }
     else if ((b1 == 'x' || b1 == 'X') && (b5 == 'x' ||
         b5 == 'X') && (b9 == 'x' || b9 == 'X')) {
@@ -75,6 +81,7 @@ function myfunc() {
         document.getElementById("b7").disabled = true;
         document.getElementById("b8").disabled = true;
         window.alert('Player X won');
+        myfunc_2();
     }
     else if ((b3 == 'x' || b3 == 'X') && (b5 == 'x' ||
         b5 == 'X') && (b7 == 'x' || b7 == 'X')) {
@@ -87,6 +94,7 @@ function myfunc() {
         document.getElementById("b8").disabled = true;
         document.getElementById("b9").disabled = true;
         window.alert('Player X won');
+        myfunc_2();
     }
     else if ((b2 == 'x' || b2 == 'X') && (b5 == 'x' ||
         b5 == 'X') && (b8 == 'x' || b8 == 'X')) {
@@ -99,6 +107,7 @@ function myfunc() {
         document.getElementById("b7").disabled = true;
         document.getElementById("b9").disabled = true;
         window.alert('Player X won');
+        myfunc_2();
     }
     else if ((b4 == 'x' || b4 == 'X') && (b5 == 'x' ||
         b5 == 'X') && (b6 == 'x' || b6 == 'X')) {
@@ -111,6 +120,7 @@ function myfunc() {
         document.getElementById("b8").disabled = true;
         document.getElementById("b9").disabled = true;
         window.alert('Player X won');
+        myfunc_2();
     }
  
     // Checking of Player X finish
@@ -127,6 +137,7 @@ function myfunc() {
         document.getElementById("b8").disabled = true;
         document.getElementById("b9").disabled = true;
         window.alert('Player 0 won');
+        myfunc_2();
     }
     else if ((b1 == '0' || b1 == '0') && (b4 == '0' ||
         b4 == '0') && (b7 == '0' || b7 == '0')) {
@@ -139,6 +150,7 @@ function myfunc() {
         document.getElementById("b8").disabled = true;
         document.getElementById("b9").disabled = true;
         window.alert('Player 0 won');
+        myfunc_2();
     }
     else if ((b7 == '0' || b7 == '0') && (b8 == '0' ||
         b8 == '0') && (b9 == '0' || b9 == '0')) {
@@ -151,6 +163,7 @@ function myfunc() {
         document.getElementById("b5").disabled = true;
         document.getElementById("b6").disabled = true;
         window.alert('Player 0 won');
+        myfunc_2();
     }
     else if ((b3 == '0' || b3 == '0') && (b6 == '0' ||
         b6 == '0') && (b9 == '0' || b9 == '0')) {
@@ -163,6 +176,7 @@ function myfunc() {
         document.getElementById("b7").disabled = true;
         document.getElementById("b8").disabled = true;
         window.alert('Player 0 won');
+        myfunc_2();
     }
     else if ((b1 == '0' || b1 == '0') && (b5 == '0' ||
         b5 == '0') && (b9 == '0' || b9 == '0')) {
@@ -175,6 +189,7 @@ function myfunc() {
         document.getElementById("b7").disabled = true;
         document.getElementById("b8").disabled = true;
         window.alert('Player 0 won');
+        myfunc_2();
     }
     else if ((b3 == '0' || b3 == '0') && (b5 == '0' ||
         b5 == '0') && (b7 == '0' || b7 == '0')) {
@@ -187,6 +202,7 @@ function myfunc() {
         document.getElementById("b8").disabled = true;
         document.getElementById("b9").disabled = true;
         window.alert('Player 0 won');
+        myfunc_2();
     }
     else if ((b2 == '0' || b2 == '0') && (b5 == '0' ||
         b5 == '0') && (b8 == '0' || b8 == '0')) {
@@ -199,6 +215,7 @@ function myfunc() {
         document.getElementById("b7").disabled = true;
         document.getElementById("b9").disabled = true;
         window.alert('Player 0 won');
+        myfunc_2();
     }
     else if ((b4 == '0' || b4 == '0') && (b5 == '0' ||
         b5 == '0') && (b6 == '0' || b6 == '0')) {
@@ -211,9 +228,10 @@ function myfunc() {
         document.getElementById("b8").disabled = true;
         document.getElementById("b9").disabled = true;
         window.alert('Player 0 won');
+        myfunc_2();
     }
  
-    // Checking of Player 0 finsh
+    // Checking of Player 0 finish
     // Here, Checking about Tie
     else if ((b1 == 'X' || b1 == '0') && (b2 == 'X'
         || b2 == '0') && (b3 == 'X' || b3 == '0') &&
@@ -224,6 +242,7 @@ function myfunc() {
             document.getElementById('print')
                 .innerHTML = "Match Tie";
             window.alert('Match Tie');
+            myfunc_2();
     }
     else {
  
@@ -260,6 +279,7 @@ flag = 1;
 function myfunc_3() {
     if (flag == 1) {
         document.getElementById("b1").value = "X";
+        document.getElementById("b1").style.color = "tomato";
         document.getElementById("b1").disabled = true;
         flag = 0;
     }
@@ -273,6 +293,7 @@ function myfunc_3() {
 function myfunc_4() {
     if (flag == 1) {
         document.getElementById("b2").value = "X";
+        document.getElementById("b2").style.color = "tomato";
         document.getElementById("b2").disabled = true;
         flag = 0;
     }
@@ -286,6 +307,7 @@ function myfunc_4() {
 function myfunc_5() {
     if (flag == 1) {
         document.getElementById("b3").value = "X";
+        document.getElementById("b3").style.color = "tomato";
         document.getElementById("b3").disabled = true;
         flag = 0;
     }
@@ -299,6 +321,7 @@ function myfunc_5() {
 function myfunc_6() {
     if (flag == 1) {
         document.getElementById("b4").value = "X";
+        document.getElementById("b4").style.color = "tomato";
         document.getElementById("b4").disabled = true;
         flag = 0;
     }
@@ -312,6 +335,7 @@ function myfunc_6() {
 function myfunc_7() {
     if (flag == 1) {
         document.getElementById("b5").value = "X";
+        document.getElementById("b5").style.color = "tomato";
         document.getElementById("b5").disabled = true;
         flag = 0;
     }
@@ -325,6 +349,7 @@ function myfunc_7() {
 function myfunc_8() {
     if (flag == 1) {
         document.getElementById("b6").value = "X";
+        document.getElementById("b6").style.color = "tomato";
         document.getElementById("b6").disabled = true;
         flag = 0;
     }
@@ -338,6 +363,7 @@ function myfunc_8() {
 function myfunc_9() {
     if (flag == 1) {
         document.getElementById("b7").value = "X";
+        document.getElementById("b7").style.color = "tomato";
         document.getElementById("b7").disabled = true;
         flag = 0;
     }
@@ -351,6 +377,7 @@ function myfunc_9() {
 function myfunc_10() {
     if (flag == 1) {
         document.getElementById("b8").value = "X";
+        document.getElementById("b8").style.color = "tomato";
         document.getElementById("b8").disabled = true;
         flag = 0;
     }
@@ -364,6 +391,7 @@ function myfunc_10() {
 function myfunc_11() {
     if (flag == 1) {
         document.getElementById("b9").value = "X";
+        document.getElementById("b9").style.color = "tomato";
         document.getElementById("b9").disabled = true;
         flag = 0;
     }
@@ -373,3 +401,20 @@ function myfunc_11() {
         flag = 1;
     }
 }
+
+
+//For timer 
+var timeleft = 20;
+var downloadTimer = setInterval(function(){
+  if(timeleft <= -1){
+    clearInterval(downloadTimer);
+    //document.getElementById("countdown").innerHTML = "Finished";
+    window.alert("Finished");
+  } else {
+    document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+    console.log(timeleft)
+    
+  }
+  document.getElementById("countdown").style.color = "tomato";
+  timeleft -= 1;
+}, 1000);
